1cabe9294eaa171b6437be8f64d0784096c6aaa7
AxelOvarath <axel.overath@student.ehb.be>
2023-12-18 13:01:01 +0100

    Fix: Signup backend
    trying to fix signup xaml and backend
    
    Reviewed-by: Axel
    Version: # 1

--- MusicDiscoveryApp/Signup.xaml.cs
+++ MusicDiscoveryApp/Signup.xaml.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+
 namespace MusicDiscoveryApp;
 
 public partial class Signup : ContentPage
@@ -12,6 +14,22 @@ public partial class Signup : ContentPage
     }
     public async void GoToRegisterInfo_Clicked(object sender, EventArgs e)
     {
+        string email = EmailEntry.ToString();
+        string password = PasswordEntry.ToString();
+
+        // Check if the user already exists
+        if (await Database.IsUserExistsAsync(email))
+            {
+                await DisplayAlert("Error", "User already exists. Please choose a different username.", "OK");
+                return;
+            }
+
+            // If the user doesn't exist, you can now proceed to insert the user into the database
+            var newUser = new User { Name = email, Password = password };
+            await Database.UsersCollection.InsertOneAsync(newUser);
+
+        // Optionally, you can navigate to the next page or display a success message
         await Navigation.PushAsync(new RegisterInfo());
+        
     }
 }

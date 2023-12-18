a60dcffed215388fb6b3d119dded416b444a942d
CaelanStraus <caelan.straus@student.ehb.be>
2023-12-18 17:50:30 +0100

    Fix: Database connection
    Mobile devices can now connect to MongoDB
    
    Reviewed-by: Caelan & Ruben
    Version: # 2

--- MusicDiscoveryApp/DataBase.cs
+++ MusicDiscoveryApp/DataBase.cs
@@ -10,8 +10,8 @@ namespace MusicDiscoveryApp
         static Database()
         {
             // Move the database setup logic to a static constructor
-            const string connectionString = "mongodb+srv://caelanstraus:PASSWORD@harmonyhuntcluster.lntmq6w.mongodb.net/?retryWrites=true&w=majority";
-            var settings = MongoClientSettings.FromConnectionString(connectionString);
+            const string connectionUri = "mongodb://caelanstraus:PASSWORD@ac-rg0cquc-shard-00-00.lntmq6w.mongodb.net:27017,ac-rg0cquc-shard-00-01.lntmq6w.mongodb.net:27017,ac-rg0cquc-shard-00-02.lntmq6w.mongodb.net:27017/?ssl=true&replicaSet=atlas-dkat5c-shard-0&authSource=admin&retryWrites=true&w=majority";
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
             settings.ServerApi = new ServerApi(ServerApiVersion.V1);
 
             string databaseName = "HormonyHDB";
--- MusicDiscoveryApp/User.cs
+++ MusicDiscoveryApp/User.cs
@@ -7,7 +7,7 @@ public class User
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     public string? Email { get; set; }
     public string? Password { get; set; }

b774ab80268157e0d75e1f9dbbc187bd2e6ec1eb
AxelOvarath <axel.overath@student.ehb.be>
2023-12-19 16:11:56 +0100

    feat: added country selection I added the option to select your country in the class RegisterInfo.xaml by using a country data package in RegisterInfo.cs

--- MusicDiscoveryApp/RegisterInfo.xaml.cs
+++ MusicDiscoveryApp/RegisterInfo.xaml.cs
@@ -1,13 +1,44 @@
-namespace MusicDiscoveryApp;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
-public partial class RegisterInfo : ContentPage
+namespace MusicDiscoveryApp
 {
-	public RegisterInfo()
-	{
-		InitializeComponent();
-	}
-    public async void GoToSwipePage_Clicked(object sender, EventArgs e)
+    public partial class RegisterInfo : ContentPage
     {
-        await Navigation.PushAsync(new Swipepage());
+        public RegisterInfo()
+        {
+            InitializeComponent();
+
+            // Populate the Picker with country names
+            PopulateCountryPicker();
+        }
+
+        private void PopulateCountryPicker()
+        {
+            // Get all countries using CultureInfo
+            CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures);
+            List<string> countryNames = new List<string>();
+
+            foreach (CultureInfo culture in cultures)
+            {
+                RegionInfo region = new RegionInfo(culture.Name);
+                if (!countryNames.Contains(region.EnglishName))
+                {
+                    countryNames.Add(region.EnglishName);
+                }
+            }
+
+            // Sort the country names
+            countryNames.Sort();
+
+            // Add country names to the Picker
+            foreach (string countryName in countryNames)
+            {
+                CountryPicker.Items.Add(countryName);
+            }
+        }
+
+        // Other methods and event handlers...
     }
 }

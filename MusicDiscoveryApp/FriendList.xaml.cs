e4858aec3185446b31de318ec00776394f05e2b6
CaelanStraus <caelan.straus@student.ehb.be>
2023-12-23 17:42:18 +0100

    feat: friend list -You can add friends by searching their username. -You can accept or deny these people. -Everything is stored in mongoDB and gets loaded in dynamically.
    reviewed-by: Caelan
    Version #3

--- MusicDiscoveryApp/FriendList.xaml.cs
+++ MusicDiscoveryApp/FriendList.xaml.cs
@@ -1,3 +1,5 @@
+using MongoDB.Driver;
+
 namespace MusicDiscoveryApp;
 
 public partial class FriendList : ContentPage
@@ -8,62 +10,80 @@ public partial class FriendList : ContentPage
         PopulateFriends();
     }
 
-    private void PopulateFriends()
+    private async void PopulateFriends()
     {
+        var currentUserUsername = UserStorage.storedUsername;
+        var currentUser = await GetUserByUsername(currentUserUsername);
 
-        string[] friends = {
-        "Emma", "Liam", "Olivia", "Noah", "Ava",
-        "William", "Sophia", "James", "Isabella", "Oliver",
-        "Mia", "Benjamin", "Charlotte", "Elijah", "Amelia",
-        "Lucas", "Harper" };
-        foreach (string friend in friends)
+        if (currentUser != null && currentUser.Friends != null && currentUser.Friends.Any())
         {
-            Frame frame = new Frame
+            foreach (var friend in currentUser.Friends)
             {
-                BackgroundColor = Colors.LightGray,
-                Margin = new Thickness(5, 5, 5, 5),
-                Padding = new Thickness(0, 10),
-                CornerRadius = 50,
-                HasShadow = true,
-                HeightRequest = 80
-            };
-
-            Label nameLabel = new Label
-            {
-                Text = friend,
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
-                VerticalOptions = LayoutOptions.CenterAndExpand
-            };
-
-            Image shareImage = new Image
-            {
-                Source = "share.svg",
-                HorizontalOptions = LayoutOptions.End,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                Margin = new Thickness(0, 0, 30, 0)
-            };
-
-            TapGestureRecognizer shareTap = new TapGestureRecognizer();
-            shareTap.Tapped += async (sender, e) =>
-            {
-                await DisplayAlert("DEBUG", friend, "OK");
-                //TODO
-            };
-            shareImage.GestureRecognizers.Add(shareTap);
-
-            frame.Content = new StackLayout
-            {
-                Orientation = StackOrientation.Horizontal,
-                Children =
-                    {
-                        nameLabel,
-                        shareImage
-                    }
-            };
-
-            FriendsStackLayout.Children.Add(frame);
+                Frame frame = CreateFriendFrame(friend);
+                FriendsStackLayout.Children.Add(frame);
+            }
+        }
+        else
+        {
+            FriendsStackLayout.Children.Add(new Label { Text = "No friends yet.", HorizontalOptions = LayoutOptions.Center });
         }
     }
+
+    private Frame CreateFriendFrame(string friend)
+    {
+        Frame frame = new Frame
+        {
+            BackgroundColor = Colors.LightGray,
+            Margin = new Thickness(5, 5, 5, 5),
+            Padding = new Thickness(0, 10),
+            CornerRadius = 50,
+            HasShadow = true,
+            HeightRequest = 80
+        };
+
+        Label nameLabel = new Label
+        {
+            Text = friend,
+            HorizontalOptions = LayoutOptions.CenterAndExpand,
+            VerticalOptions = LayoutOptions.CenterAndExpand
+        };
+
+        Image shareImage = new Image
+        {
+            Source = "share.svg",
+            HorizontalOptions = LayoutOptions.End,
+            VerticalOptions = LayoutOptions.CenterAndExpand,
+            Margin = new Thickness(0, 0, 30, 0)
+        };
+
+        TapGestureRecognizer shareTap = new TapGestureRecognizer();
+        shareTap.Tapped += async (sender, e) =>
+        {
+            await DisplayAlert("DEBUG", friend, "OK");
+            //TODO: Handle share action
+        };
+        shareImage.GestureRecognizers.Add(shareTap);
+
+        frame.Content = new StackLayout
+        {
+            Orientation = StackOrientation.Horizontal,
+            Children =
+        {
+            nameLabel,
+            shareImage
+        }
+        };
+
+        return frame;
+    }
+
+    private async Task<User?> GetUserByUsername(string username)
+    {
+        var filter = Builders<User>.Filter.Eq(u => u.Username, username);
+        var user = await Database.UsersCollection.Find(filter).FirstOrDefaultAsync();
+        return user;
+    }
+
 
     public async void GoToFriendAdd_Clicked(object sender, EventArgs e)
 	{
